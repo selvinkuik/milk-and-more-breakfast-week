f03860c256e17a8c9b3e28c64399907b290bafc4
Sel-Vin Kuik <selvinkuik@gmail.com>
2022-01-05 11:41:13 +0000

    Updating grid

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 import '../scss/app.scss'
+
+// document.querySelector('form').addEventListener('submit', () => {
+//   var data = new FormData()
+//   data.append('Email', document.querySelector('input[name="Email"]').value)
+//   data.append('formSourceName', document.querySelector('input[name="formSourceName"]').value)
+//   data.append('sp_exp', document.querySelector('input[name="sp_exp"]').value)
+//   fetch(document.querySelector('form').action, { method: 'POST', body: data })
+// })
